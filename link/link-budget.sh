25e58301d100f64068f7d07e0df47d82aed01516
Gary Allen <gvcallen@gmail.com>
2023-09-28 19:28:31 +0200

    Worked on report and added link budget script

---
+++ link/link-budget.sh
@@ -0,0 +1,23 @@
+link-budget \
+   --freq 433e6 \
+   --bw 1e6 \
+#    Saldanha
+   --sat-lat -33.0277 \
+   --sat-long 17.9176 \
+#    Worcester
+#    --sat-lat -33.6465 \
+#    --sat-long 19.4485 \
+   --sat-alt 30000 \
+   --eirp 12 \
+   --lnb-noise-fig 0.6 \
+   --lnb-gain 40 \
+   --coax-length 1.5 \
+   --rx-dish-gain 10 \
+   --rx-noise-fig 2 \
+#    Signal Hill
+   --rx-lat -33.9173 \
+   --rx-long 18.4039 \
+#    Darling
+#    --rx-lat -33.44 \
+#    --rx-long 18.52 \
+   --rx-height 350
